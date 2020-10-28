b3b390b7cf485ac271f63e2fed82410b1a8d2049
kanticha0707 <72332883+kanticha0707@users.noreply.github.com>
2020-10-28 17:37:46 +0700

    61050812 กันติชา  มณีกุล

---
+++ atm/src/main/java/th/ac/kmitl/atm/Service/CustomerService.java
@@ -0,0 +1,63 @@
+package th.ac.kmitl.atm.Service;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+import th.ac.kmitl.atm.Model.Customer;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerService {
+    private ArrayList<Customer> customerList;
+
+
+    @PostConstruct
+    public void postConstruct() {
+        customerList = new ArrayList<>();
+    }
+
+    public void createCustomer(Customer customer) {
+        // .... hash pin ....
+        String hashPin = hash(customer.getPin());
+        customer.setPin(hashPin);
+        customerList.add(customer);
+
+    }
+
+    public List<Customer> getCustomers() {
+        return new ArrayList<>(this.customerList);
+    }
+
+    private String hash(String pin) {
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(pin, salt);
+    }
+
+    public Customer findCustomer(int id) {
+        for (Customer customer : customerList) {
+            if (customer.getId() == id)
+                return customer;
+        }
+        return null;
+
+
+    }
+    public Customer checkPin(Customer inputCustomer) {
+        // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
+        Customer storedCustomer = findCustomer(inputCustomer.getId());
+
+        // 2. ถ้ามี id ตรง ให้เช็ค pin ว่าตรงกันไหม โดยใช้ฟังก์ชันเกี่ยวกับ hash
+        if (storedCustomer != null) {
+            String hashPin = storedCustomer.getPin();
+
+            if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
+                return storedCustomer;
+        }
+        // 3. ถ้าไม่ตรง ต้องคืนค่า null
+        return null;
+    }
+
+
+}
